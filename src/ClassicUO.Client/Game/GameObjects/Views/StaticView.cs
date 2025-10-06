e0c86acd6eb528cd9528eee2b7c2da50fc7c7057
TazmanianTad <bittiez@gmail.com>
2025-10-05 21:55:50 -0600

    Static view improvements

--- src/ClassicUO.Client/Game/GameObjects/Views/StaticView.cs
+++ src/ClassicUO.Client/Game/GameObjects/Views/StaticView.cs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 
+using System;
+using System.Runtime.CompilerServices;
 using ClassicUO.Assets;
 using ClassicUO.Configuration;
 using ClassicUO.Game.Data;
@@ -12,8 +14,32 @@ namespace ClassicUO.Game.GameObjects
 {
     public sealed partial class Static
     {
+        private const int MAX_Z_DIFFERENCE_FOR_DISPLAY_RADIUS = 11;
+        private const int LIGHT_OFFSET = 22;
+
         private int _canBeTransparent;
 
+        /// <summary>
+        /// Gets the display graphic for this static, applying tree-to-stump replacement if enabled.
+        /// </summary>
+        /// <param name="graphic">The base graphic to process.</param>
+        /// <returns>The graphic to display, either the original or replacement graphic.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private ushort GetDisplayGraphic(ushort graphic)
+        {
+            if (StaticFilters.IsTree(graphic, out _) && _profile?.TreeToStumps == true)
+            {
+                return Constants.TREE_REPLACE_GRAPHIC;
+            }
+            return graphic;
+        }
+
+        /// <summary>
+        /// Determines whether this static object should be rendered with transparency
+        /// based on its height relative to the specified Z coordinate.
+        /// </summary>
+        /// <param name="z">The Z coordinate to test against.</param>
+        /// <returns>True if the object should be transparent, false otherwise.</returns>
         public override bool TransparentTest(int z)
         {
             bool r = true;
@@ -30,12 +56,22 @@ namespace ClassicUO.Game.GameObjects
             return r;
         }
 
+        /// <summary>
+        /// Renders the static object to the screen.
+        /// </summary>
+        /// <param name="batcher">The rendering batch manager.</param>
+        /// <param name="posX">The X screen position.</param>
+        /// <param name="posY">The Y screen position.</param>
+        /// <param name="depth">The rendering depth for sorting.</param>
+        /// <returns>True if the object was rendered, false if skipped.</returns>
         public override bool Draw(UltimaBatcher2D batcher, int posX, int posY, float depth)
         {
             if (!AllowedToDraw || IsDestroyed)
             {
                 return false;
             }
+
+            var scene = Client.Game.GetScene<GameScene>();
 
             ushort graphic = Graphic;
             ushort hue = Hue;
@@ -55,7 +91,7 @@ namespace ClassicUO.Game.GameObjects
                 hue = Constants.OUT_RANGE_COLOR;
                 partial = false;
             }
-            else if (World.Player.IsDead && _profile.EnableBlackWhiteEffect)
+            else if (World.Player != null && World.Player.IsDead && _profile.EnableBlackWhiteEffect)
             {
                 hue = Constants.DEAD_RANGE_COLOR;
                 partial = false;
@@ -71,17 +107,18 @@ namespace ClassicUO.Game.GameObjects
                 hue = SpellVisualRangeManager.Instance.ProcessHueForTile(hue, this);
             }
 
-            if (_profile.DisplayRadius && Distance == _profile.DisplayRadiusDistance && System.Math.Abs(Z - World.Player.Z) < 11)
+            if (_profile.DisplayRadius &&
+                Distance == _profile.DisplayRadiusDistance &&
+                World.Player != null &&
+                Math.Abs(Z - World.Player.Z) < MAX_Z_DIFFERENCE_FOR_DISPLAY_RADIUS)
+            {
                 hue = _profile.DisplayRadiusHue;
+            }
 
             Vector3 hueVec = ShaderHueTranslator.GetHueVector(hue, partial, AlphaHue / 255f);
 
+            graphic = GetDisplayGraphic(graphic);
             bool isTree = StaticFilters.IsTree(graphic, out _);
-
-            if (isTree && _profile.TreeToStumps)
-            {
-                graphic = Constants.TREE_REPLACE_GRAPHIC;
-            }
 
             DrawStaticAnimated(
                 batcher,
@@ -96,47 +133,47 @@ namespace ClassicUO.Game.GameObjects
                 _profile.AnimatedWaterEffect && ItemData.IsWet
             );
 
-            if (_isLight)
+            if (_isLight && scene != null)
             {
-                Client.Game.GetScene<GameScene>().AddLight(this, this, posX + 22, posY + 22);
+                scene.AddLight(this, this, posX + LIGHT_OFFSET, posY + LIGHT_OFFSET);
             }
 
             return true;
         }
 
+        /// <summary>
+        /// Checks if the mouse cursor is over this static object using pixel-perfect collision detection.
+        /// </summary>
+        /// <returns>True if the mouse is over a non-transparent pixel of the object, false otherwise.</returns>
         public override bool CheckMouseSelection()
         {
-            if (
-                !(
-                    SelectedObject.Object == this
-                    || FoliageIndex != -1
-                        && Client.Game.GetScene<GameScene>().FoliageIndex == FoliageIndex
-                )
-            )
+            // Early return if already selected
+            if (SelectedObject.Object == this)
             {
-                ushort graphic = Graphic;
-
-                bool isTree = StaticFilters.IsTree(graphic, out _);
-
-                if (isTree && _profile.TreeToStumps)
-                {
-                    graphic = Constants.TREE_REPLACE_GRAPHIC;
-                }
-
-                ref var index = ref Client.Game.UO.FileManager.Arts.File.GetValidRefEntry(graphic + 0x4000);
-
-                Point position = RealScreenPosition;
-                position.X -= index.Width;
-                position.Y -= index.Height;
-
-                return Client.Game.UO.Arts.PixelCheck(
-                    graphic,
-                    SelectedObject.TranslatedMousePositionByViewport.X - position.X,
-                    SelectedObject.TranslatedMousePositionByViewport.Y - position.Y
-                );
+                return false;
             }
 
-            return false;
+            // Early return if this is foliage and matches the current foliage index
+            var scene = Client.Game.GetScene<GameScene>();
+            if (FoliageIndex != -1 && scene != null && scene.FoliageIndex == FoliageIndex)
+            {
+                return false;
+            }
+
+            // Get the display graphic (with tree-to-stump replacement if needed)
+            ushort graphic = GetDisplayGraphic(Graphic);
+
+            ref var index = ref Client.Game.UO.FileManager.Arts.File.GetValidRefEntry(graphic + 0x4000);
+
+            Point position = RealScreenPosition;
+            position.X -= index.Width;
+            position.Y -= index.Height;
+
+            return Client.Game.UO.Arts.PixelCheck(
+                graphic,
+                SelectedObject.TranslatedMousePositionByViewport.X - position.X,
+                SelectedObject.TranslatedMousePositionByViewport.Y - position.Y
+            );
         }
     }
 }
